94d60445dde988ac0fa6865b9190833f186a55ce
Jorge2610 <jorge261093@gmail.com>
2022-10-07 22:42:05 -0400

    Correciones iniciales.

---
+++ src/DataBase/Recipe.java
@@ -0,0 +1,34 @@
+package DataBase;
+
+public class Recipe {
+    private int id,
+                score;
+    private String nameRecipe,
+                   ingredients;
+    
+    
+    
+    public Recipe(String nameRecipe, String ingredients, int score){
+        this.id = 0; 
+        this.nameRecipe = nameRecipe;
+        this.ingredients = ingredients;
+        this.score = score;   
+    }
+    
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNameRecipe() {
+        return nameRecipe;
+    }
+    
+    public String getIngredients() {
+        return ingredients;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+}
