5841f20b9d172a644d7fc1d257073b1d4018f606
jorge ledezma <jledezma@mail.com>
2022-10-09 21:31:54 -0400

    actualizacion de ventana jlz

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import recepieView.ventJLZE;
 
@@ -6,22 +6,22 @@ import recepieView.ventJLZE;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-=======
+
 import recipesViews.IvanPalacios.RecipeGUI;
 import recipesViews.jorgeS.Receta;
->>>>>>> master
+
 
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         // TODO code application logic here
-        new ventJLZE().setVisible(true);
-=======
+        
+
         
         new RecipeGUI().setVisible(true);
         new Receta();
->>>>>>> master
+
     }
    
 }
