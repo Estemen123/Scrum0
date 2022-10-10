3ec4c46942caa029177cd8d9478ca930334d1475
“AckDot” <vargasorellanajoseroberto@gmail.com>
2022-10-10 18:59:13 -0400

    Problemas del merge solucionados , todas las interfaces estan funcionando correctamente

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
-<<<<<<< HEAD
-=======
+
+
 
 
 
@@ -8,23 +8,18 @@
 //import recepiesView.ventJLZE;
 
 
->>>>>>> origin/roberto
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
-<<<<<<< HEAD
+
 import recipesViews.Denis.Controller;
-import recipesViews.IvanPalacios.RecipeGUI;
 import recipesViews.jorgeS.Receta;
 import recipesViews.josue.ventanaJosue;
-=======
 import recipesViews.roberto.RecetasGUI;
-import recipesViews.Denis.Controller;
 import recipesViews.IvanPalacios.RecipeGUI;
-import recipesViews.jorgeS.Receta;
->>>>>>> origin/roberto
 import recipesViews.ledezma.winJLZ;
 
 
@@ -39,15 +34,15 @@ public class Main {
         //new winJLZ().setVisible(true);
         //new RecipeGUI().setVisible(true);
         //new Receta();
-<<<<<<< HEAD
+
 
         new winJLZ().setVisible(true);
         new RecipeGUI().setVisible(true);
         new Receta();
         new ventanaJosue().setVisible(true);
-=======
+
          new RecetasGUI().setVisible(true);
->>>>>>> origin/roberto
+
 
     }
    
